bf26d77c4e32c7c81b7a666e22be3981e9d15a83
JasonLunsford <jl630043@gmail.com>
2017-09-01 08:08:57 -0400

    added a value to document schema

--- models/themes.js
+++ models/themes.js
@@ -2,9 +2,10 @@ var mongoose = require("mongoose"),
 	Schema = mongoose.Schema;
 
 var ThemesSchema = new Schema({
+	name  		: { type: String, required: true, trim: true },
 	parent_name : { type: String, trim: true },
-	set_count   : { type: Number, min: [0, 'Invalid set count'] },
-	theme_name  : { type: String, required: true, trim: true }
+	parent_id   : { type: Schema.ObjectId, ref: 'Themes', default: 0 },
+	set_count   : { type: Number, min: [0, 'Invalid set count'] }
 });
 
 module.exports = mongoose.model('Themes', ThemesSchema, 'themes');
