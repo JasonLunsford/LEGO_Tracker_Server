e44937af15d48f2e1ce9e322a7948caaefbf14a5
JasonLunsford <jl630043@gmail.com>
2017-11-06 07:09:19 -0500

    Expanded schema to include arrays for member sets, alternate set numbers, and a flag for whether the set also includes gear, aka any non LEGO piece like a water bottle, etc

--- models/sets.js
+++ models/sets.js
@@ -4,32 +4,39 @@ const mongoose = require('mongoose'),
 /***********
 * Field Definitions:
 *
+* alt_set_nums: alternate set numbers for the same EXACT set
 * build_urls: collection of set build instructions possible with this specific set
+* has_gear: flag if the set includes non-LEGO piece stuff like water bottles, frisbees, etc
+* members: other sets belonging to this set (if this array populated set is now a collection)
 * num_minifig: the number of mini-figures found in this set, if any
 ***********/
 
-// external data
-let UrlsSchema = new Schema({
-	source      : { type: String, lowercase: true, trim: true },
-	url         : { type: String, lowercase: true, trim: true }
-});
-
+// array schemas
 let SetPiecesSchema = new Schema({
-	element_id  : { type: Object },
-	is_spare    : { type: Boolean, default: false },
-	quantity    : { type: Number, min: [0, 'Invalid quantity number'] }
+	element_id   : { type: Object },
+	is_spare     : { type: Boolean, default: false },
+	quantity     : { type: Number, min: [0, 'Invalid quantity number'] }
 });
 
+let UrlsSchema = new Schema({
+	source       : { type: String, lowercase: true, trim: true },
+	url          : { type: String, lowercase: true, trim: true }
+});
+
+// main schema
 let SetsSchema = new Schema({
-	build_urls  : [UrlsSchema],
-	name        : { type: String, required: true, lowercase: true, trim: true },
-	num_minifig : { type: Number, min: [0, 'Invalid number of mini-figures'], default: 0 },
-	num_pieces  : { type: Number, min: [0, 'Invalid number of pieces'] },
-	set_img_urls: [UrlsSchema],
-	set_num     : { type: String, required: true, lowercase: true, trim: true },
-	set_pieces  : [SetPiecesSchema],
-	theme_id    : { type: Object, required: true },
-	year        : { type: Number, min: [1958, 'Invalid year'], max: [9999, 'Invalid year'] }
+	alt_set_nums : [{ type: Object }],
+	build_urls   : [UrlsSchema],
+	has_gear     : { type: Boolean, default: false },
+	name         : { type: String, required: true, lowercase: true, trim: true },
+	members      : [{ type: Object }],
+	num_minifig  : { type: Number, min: [0, 'Invalid number of mini-figures'], default: 0 },
+	num_pieces   : { type: Number, min: [0, 'Invalid number of pieces'] },
+	set_img_urls : [UrlsSchema],
+	set_num      : { type: String, required: true, lowercase: true, trim: true },
+	set_pieces   : [SetPiecesSchema],
+	theme_id     : { type: Object, required: true },
+	year         : { type: Number, min: [1958, 'Invalid year'], max: [9999, 'Invalid year'] }
 });
 
 // Indexing all fields of type String, referenced as text
